bf4ca8d54091937d706cc9d6bd44d74ae1efb9c9
fedeesti <fede.estigarribia@hotmail.com>
2023-01-29 18:45:45 -0300

    back: Add get team by id
    The route to get equipment is added next to its tests.

--- server/__test__/app.test.ts
+++ server/__test__/app.test.ts
@@ -1,12 +1,36 @@
 import request from 'supertest';
 import { app } from '../app';
 
-// const api = supertest(app);
+const api = request(app);
 
 describe('GET /teams', () => {
-  test('teams are returned as json', async () => {
-    const res = await request(app).get('/api/v1/teams');
+  test('Teams are returned as json', async () => {
+    const res = await api.get('/api/v1/teams');
+
     expect(res.status).toEqual(200);
     expect(res.headers['content-type']).toMatch(/application\/json/);
+  });
+});
+
+describe('GET /teams/:id', () => {
+  test('Team is returned as json', async () => {
+    const res = await api.get('/api/v1/teams/64');
+
+    expect(res.status).toEqual(200);
+    expect(res.headers['content-type']).toMatch(/application\/json/);
+  });
+
+  test('Id does not match a team', async () => {
+    const res = await api.get('/api/v1/teams/-1');
+
+    expect(res.status).toEqual(404);
+    expect(res.body.message).toBe('Team not found');
+  });
+
+  test('Get incorrect ID', async () => {
+    const res = await api.get('/api/v1/teams/asd');
+
+    expect(res.status).toEqual(400);
+    expect(res.body.message).toBe('Bad Request');
   });
 });
--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express, { Express, Response, Request } from 'express';
+import { Team } from './types/team';
 import teams from '../data/equipos.db.json';
 
 export const app: Express = express();
@@ -7,4 +8,20 @@ app.use(express.json());
 
 app.get('/api/v1/teams', (_req: Request, res: Response) => {
   res.send(teams);
+});
+
+app.get('/api/v1/teams/:id', (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+
+  if (!id) return res.status(400).json({ message: 'Bad Request' });
+
+  try {
+    const team: Team | undefined = teams.find((team) => team.id === id);
+
+    if (!team) return res.status(404).json({ message: 'Team not found' });
+
+    return res.json(team);
+  } catch (err) {
+    return res.status(500).json({ message: 'Internal server errror' });
+  }
 });
