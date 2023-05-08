c05f299d244732f30b6a6416c49084445d934b27
fedeesti <fede.estigarribia@hotmail.com>
2023-05-08 16:07:13 -0300

    Correct port to test A port for testing different from the production port is added to avoid modifying the equipos.db.json file

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,11 @@
 import express, { Express } from 'express';
 import apiRouter from './routes/index.routes';
 
-export const app: Express = express();
+export function createApp() {
+  const app: Express = express();
 
-app.use(express.json());
-app.use('/api/v1', apiRouter);
+  app.use(express.json());
+  app.use('/api/v1', apiRouter);
+
+  return app;
+}
