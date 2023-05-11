80c7862736dd100cdbb8c712e2f70e6e8dfdc120
fedeesti <fede.estigarribia@hotmail.com>
2023-05-11 12:06:08 -0300

    Add create team
    Create team is added next to its test.

--- server/routes/teams.routes.ts
+++ server/routes/teams.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { deleteTeamById, getTeamById, getTeams } from '../controllers/teams';
+import { createTeam, deleteTeamById, getTeamById, getTeams } from '../controllers/teams';
 
 const router: Router = Router();
 
-router.route('/').get(getTeams);
+router.route('/').get(getTeams).post(createTeam);
 
 router.route('/:id').get(getTeamById).delete(deleteTeamById);
 
