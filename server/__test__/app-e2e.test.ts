8a63163237d828ea550bdb1f199ebaf872b76155
fedeesti <fede.estigarribia@hotmail.com>
2023-05-22 12:41:47 -0300

    Add schema to validate team update
    The validators are added to update a team and tests are added to verify that the validators work correctly.

--- server/__test__/app-e2e.test.ts
+++ server/__test__/app-e2e.test.ts
@@ -59,50 +59,432 @@ describe('Teams Service', () => {
   });
 
   describe('UPDATE by ID', () => {
-    test('should respond with a 400 status code and show the message Bad Request', async () => {
-      const errorsData = [{ field: 'id', message: 'ID is wrong, its must be a number' }];
+    describe('Update a team successfully', () => {
+      test('when update team, should respond with a message that has been updated', async () => {
+        const updateTeam = {
+          address: 'Piazzale Angelo Moratti, 20151',
+          phone: '+44 (012) 726300',
+          website: 'https://www.acmilan.com/',
+          email: 'admin@acmilan.com',
+          founded: 1899,
+          venue: 'Calcistico San Siro',
+        };
 
-      const { statusCode, body } = await api.put('/api/v1/teams/asd');
+        const { statusCode, body } = await api
+          .put('/api/v1/teams/64')
+          .send(updateTeam)
+          .set('Accept', 'application/json');
 
-      expect(statusCode).toEqual(400);
-      expect(body.message).toBe('Bad Request');
-      expect(body.data).toEqual(expect.arrayContaining(errorsData));
+        expect(statusCode).toEqual(200);
+        expect(body).toBe('the team with ID: 64 has been updated');
+      });
     });
-    test('should respond with a 404 status code and show the message Team not found', async () => {
-      const { statusCode, body } = await api.put('/api/v1/teams/99999999999');
+    describe('Wrong fields', () => {
+      test('when the required fields are empty, it should respond with an array of error details', async () => {
+        const dataErrors = [
+          {
+            field: 'area.name',
+            message: 'Country cannot be empty',
+          },
+          {
+            field: 'name',
+            message: 'Name cannot be empty',
+          },
+          {
+            field: 'shortName',
+            message: 'ShortName cannot be empty',
+          },
+          {
+            field: 'tla',
+            message: 'TLA cannot be empty',
+          },
+          {
+            field: 'crestUrl',
+            message: 'Image cannot be empty',
+          },
+          {
+            field: 'clubColors',
+            message: 'clubColors cannot be empty',
+          },
+        ];
+        const teamWithEmptyFields = {
+          area: {
+            name: '',
+          },
+          name: '',
+          shortName: '',
+          tla: '',
+          crestUrl: '',
+          clubColors: '',
+        };
 
-      expect(statusCode).toEqual(404);
-      expect(body.message).toBe('Team not found');
-    });
-    // beforeEach(async () => {
-    //   const addTeam = {
-    //     area: {
-    //       name: 'Italy',
-    //     },
-    //     name: 'AC Milán',
-    //     shortName: 'Milán',
-    //     tla: 'MIL',
-    //     crestUrl: 'https://upload.wikimedia.org/wikipedia/en/0/0c/Leeds_United.svg',
-    //     clubColors: 'Red / Black',
-    //   };
+        const { statusCode, body } = await api
+          .put('/api/v1/teams/64')
+          .send(teamWithEmptyFields)
+          .set('Accept', 'application/json');
 
-    //   await api.post('/api/v1/teams').send(addTeam).set('Accept', 'application/json');
-    // });
+        expect(statusCode).toEqual(400);
+        expect(body.message).toBe('Bad Request');
+        expect(body.data).toEqual(dataErrors);
+      });
+      test('when the email is invalid it should throw error details', async () => {
+        const invalidEmail = [
+          {
+            email: 'http://www.acmilan.com',
+          },
+          {
+            email: 'hola@com',
+          },
+          {
+            email: 'hola',
+          },
+          {
+            email: '1234',
+          },
+        ];
 
-    test('when the team updated correctly, should return a message that it has updated successfully', async () => {
-      const fieldsToUpdate = {
-        address: 'Meazza Road_Milan L4 OTH',
-        phone: '+44 (0871) 9841955',
-        website: 'http://www.acmilan.com',
-        email: 'acmilan@contac.com',
-        founded: 1905,
-        venue: 'San Siro',
-      };
+        const dataError = [
+          {
+            field: 'email',
+            message: 'Please provide valid email',
+          },
+        ];
 
-      const { statusCode, body } = await api.put('/api/v1/teams/64').send(fieldsToUpdate);
+        for (let i = 0; i < invalidEmail.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidEmail[i])
+            .set('Accept', 'application/json');
 
-      expect(statusCode).toEqual(200);
-      expect(body).toBe('the team with ID: 64 has been updated');
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+          expect(body.data).toEqual(expect.arrayContaining(dataError));
+        }
+      });
+      test('when the phone is invalid it should throw error details', async () => {
+        const invalidPhone = [{ phone: '+44  hola 9841955' }, { phone: '+44 (0871) hola' }, { phone: 'hola' }];
+
+        const dataError = [
+          {
+            field: 'phone',
+            message: 'Phone should be a valid phone number',
+          },
+        ];
+
+        for (let i = 0; i < invalidPhone.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidPhone[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+          expect(body.data).toEqual(expect.arrayContaining(dataError));
+        }
+      });
+      test('when the website is invalid it should throw error details', async () => {
+        const invalidWebsite = [
+          { website: 'www.acmil@angmail.com' },
+          { website: 'mailto://acmilan.com' },
+          { website: 'hola' },
+          { website: 'fdp://www.acmilan.com' },
+          { website: 'htps://www.acmilan.com' },
+        ];
+
+        const dataError = [
+          {
+            field: 'website',
+            message: 'Website should be a URL',
+          },
+        ];
+
+        for (let i = 0; i < invalidWebsite.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidWebsite[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+          expect(body.data).toEqual(expect.arrayContaining(dataError));
+        }
+      });
+      test('when the address is invalid it should throw error details', async () => {
+        const invalidAddress = [
+          { address: 'hola@hola.com' },
+          { address: 'mailto://acmilan.com' },
+          { address: 'Leeds_United.svg' },
+          { address: 'AC-Milan 4042' },
+          { address: 'hola' },
+          { address: 'P.sherman calle wallaby 42 sydney  Pelicula Buscando a Nemo' },
+        ];
+
+        const dataError = [
+          {
+            field: 'address',
+            message: 'Address should be a valid address',
+          },
+          {
+            field: 'address',
+            message: 'Address should be at least 7 chars long and maximum of 40 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidAddress.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidAddress[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 3
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the TLA is invalid it should throw error details', async () => {
+        const invalidTla = [
+          {
+            tla: '19',
+          },
+          {
+            tla: 'h@l',
+          },
+          {
+            tla: '-12',
+          },
+          {
+            tla: 'hola',
+          },
+        ];
+
+        const dataError = [
+          {
+            field: 'tla',
+            message: 'TLA only contains letters',
+          },
+          {
+            field: 'tla',
+            message: 'TLA must contain 3 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidTla.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidTla[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 2
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the clubColors is invalid it should throw error details', async () => {
+        const invalidClubColors = [
+          {
+            clubColors: 'red . black',
+          },
+          {
+            clubColors: 'red @ black',
+          },
+          {
+            clubColors: 'red _ black',
+          },
+          {
+            clubColors: 'red - black',
+          },
+          {
+            clubColors: 'red / black / white / orange / blue',
+          },
+        ];
+
+        const dataError = [
+          {
+            field: 'clubColors',
+            message: 'clubColors only contains words, spaces and slash',
+          },
+          {
+            field: 'clubColors',
+            message: 'clubColors should be at least 3 chars long and maximum of 20 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidClubColors.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidClubColors[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 3
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the venue is invalid it should throw error details', async () => {
+        const invalidVenue = [
+          { venue: 'hola@hotmail.com' },
+          { venue: 'https://www.acmilan.com' },
+          { venue: '-Milan' },
+          { venue: 'Milan-' },
+          { venue: '.Milan' },
+          { venue: 'Milan.' },
+          { venue: 'Milan_' },
+          { venue: '_Milan' },
+          { venue: 'AC / Milan' },
+          { venue: 'San' },
+          { venue: 'Lorem ipsum dolor sit amet consectetur adipiscing elit' },
+        ];
+
+        const dataError = [
+          {
+            field: 'venue',
+            message: "Venue should only contain alphanumeric, spaces and the next symbol: '-'",
+          },
+          {
+            field: 'venue',
+            message: 'Venue should be at least 7 chars long and maximum of 40 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidVenue.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidVenue[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 8
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the name is invalid it should throw error details', async () => {
+        const invalidName = [
+          { name: 'hola@hotmail.com' },
+          { name: 'https://www.acmilan.com' },
+          { name: '-Milan' },
+          { name: 'Milan-' },
+          { name: '.Milan' },
+          { name: 'Milan.' },
+          { name: 'Milan_' },
+          { name: '_Milan' },
+          { name: 'Milan' },
+          { name: 'Lorem ipsum dolor sit amet cons' },
+        ];
+
+        const dataError = [
+          {
+            field: 'name',
+            message: "Name should only contain alphanumeric, spaces and the next symbols: [ '/', '.', '-']",
+          },
+          {
+            field: 'name',
+            message: 'Name should be at least 7 chars long and maximum of 30 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidName.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidName[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 7
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the shortName is invalid it should throw error details', async () => {
+        const invalidShortName = [
+          { shortName: 'hola@hotmail.com' },
+          { shortName: 'https://www.acmilan.com' },
+          { shortName: '-Milan' },
+          { shortName: 'Milan-' },
+          { shortName: '.Milan' },
+          { shortName: 'Milan.' },
+          { shortName: 'Milan_' },
+          { shortName: '_Milan' },
+          { shortName: 'Mil' },
+          { shortName: 'Lorem ipsum dolor sit' },
+        ];
+
+        const dataError = [
+          {
+            field: 'shortName',
+            message: "ShortName should only contain alphanumeric, spaces and the next symbols: [ '/', '.', '-']",
+          },
+          {
+            field: 'shortName',
+            message: 'ShortName should be at least 4 chars long and maximum of 20 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidShortName.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidShortName[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 7
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the founded is invalid it should throw error details', async () => {
+        const invalidFounded = [
+          { founded: 'hola' },
+          { founded: '904-' },
+          { founded: '-904' },
+          { founded: '.904' },
+          { founded: '904.' },
+          { founded: '904_' },
+          { founded: '_904' },
+          { founded: '20133' },
+          { founded: '14' },
+        ];
+
+        const dataError = [
+          {
+            field: 'founded',
+            message: 'Founded must not contain symbols or alphabetic',
+          },
+          {
+            field: 'founded',
+            message: 'Founded must contain 4 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidFounded.length; i++) {
+          const { statusCode, body } = await api
+            .put('/api/v1/teams/64')
+            .send(invalidFounded[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 6
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
     });
   });
 
@@ -172,7 +554,7 @@ describe('Teams Service', () => {
       });
     });
     describe('Missing or wrong fields', () => {
-      test('when the required fields are not specified, it should respond with an array of error details.', async () => {
+      test('when the required fields are not specified, it should respond with an array of error details', async () => {
         const teamWithoutRequiredData = {
           website: 'http://www.acmilan.com',
           founded: 1905,
@@ -303,7 +685,7 @@ describe('Teams Service', () => {
           { address: 'Leeds_United.svg' },
           { address: 'AC-Milan 4042' },
           { address: 'hola' },
-          { address: 'P.sherman calle wallaby 42 sydney  Buscando a Nemo' },
+          { address: 'P.sherman calle wallaby 42 sydney  Pelicula Buscando a Nemo' },
         ];
 
         const dataError = [
@@ -530,6 +912,44 @@ describe('Teams Service', () => {
           expect(body.message).toBe('Bad Request');
 
           i <= 7
+            ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
+            : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
+        }
+      });
+      test('when the founded is invalid it should throw error details', async () => {
+        const invalidFounded = [
+          { founded: 'hola' },
+          { founded: '904-' },
+          { founded: '-904' },
+          { founded: '.904' },
+          { founded: '904.' },
+          { founded: '904_' },
+          { founded: '_904' },
+          { founded: '20133' },
+          { founded: '14' },
+        ];
+
+        const dataError = [
+          {
+            field: 'founded',
+            message: 'Founded must not contain symbols or alphabetic',
+          },
+          {
+            field: 'founded',
+            message: 'Founded must contain 4 chars',
+          },
+        ];
+
+        for (let i = 0; i < invalidFounded.length; i++) {
+          const { statusCode, body } = await api
+            .post('/api/v1/teams')
+            .send(invalidFounded[i])
+            .set('Accept', 'application/json');
+
+          expect(statusCode).toEqual(400);
+          expect(body.message).toBe('Bad Request');
+
+          i <= 6
             ? expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[0])]))
             : expect(body.data).toEqual(expect.arrayContaining([expect.objectContaining(dataError[1])]));
         }
--- server/routes/teams.routes.ts
+++ server/routes/teams.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { checkSchema } from 'express-validator';
 import { createTeam, deleteTeamById, getTeamById, getTeams, updateTeamById } from '../controllers/teams';
-import { idSchema, createTeamSchema } from '../schemas/team.schema';
+import { idSchema, createTeamSchema, updateTeamSchema } from '../schemas/team.schema';
 import { validatorHandler } from '../middlewares/validator.handler';
 
 const router: Router = Router();
@@ -11,7 +11,7 @@ router.route('/').get(getTeams).post(checkSchema(createTeamSchema), validatorHan
 router
   .route('/:id')
   .get(checkSchema(idSchema), validatorHandler, getTeamById)
-  .put(checkSchema(idSchema), validatorHandler, updateTeamById)
+  .put(checkSchema(updateTeamSchema), validatorHandler, updateTeamById)
   .delete(checkSchema(idSchema), validatorHandler, deleteTeamById);
 
 export default router;
